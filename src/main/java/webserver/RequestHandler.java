e6903cb8ac8287ba74e1e84a6357bbb2445aaa4d
moving-line <movinglinecheck@gmail.com>
2020-04-05 22:57:09 +0900

    요구사항 2 구현
    - Get Method로 회원가입 구현

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -3,12 +3,15 @@ package webserver;
 import java.io.*;
 import java.net.Socket;
 import java.nio.file.Files;
+import java.util.Map;
 
+import model.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import static java.nio.charset.StandardCharsets.*;
 import static util.HttpRequestUtils.getUrl;
+import static util.HttpRequestUtils.parseQueryString;
 
 public class RequestHandler extends Thread {
     private static final Logger log = LoggerFactory.getLogger(RequestHandler.class);
@@ -29,10 +32,17 @@ public class RequestHandler extends Thread {
             if(line == null) {
                 return;
             }
+
             String url = getUrl(line);
 
-            while(!line.equals("")) {
-                line = br.readLine();
+            if(url.startsWith("/user/create")) {
+                int index = url.indexOf("?");
+                String queryString = url.substring(index + 1);
+                Map<String, String> map = parseQueryString(queryString);
+                User user = new User(map.get("userId"), map.get("password"), map.get("name"), map.get("email"));
+                log.debug("User : {}", user);
+
+                url = "/index.html";
             }
 
             DataOutputStream dos = new DataOutputStream(out);
